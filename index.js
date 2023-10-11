a4a15dc2eb0d7c138ceed8570379f143196d5748
evansthitai <evansnt9619@gmail.com>
2023-10-11 10:49:10 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const friends = ["Charlie","Samip","Ali"]
+
+function writeCards (){
+    for (let i = 0; i< friends.length; i++){
+        console.log('Thank you,' + friends[i] +" " + "for the wonderful birthday gift" )
+         }
+        return friends
+    }
+
+    writeCards(friends);
+
+    let x = 10
+
+    while (x > 0){
+        console.log(x--) ;
+    }
